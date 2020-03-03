5cccc45e3ffcdc02a447344df5db3325fd9f384d
YK911 <jaroslavkositsa@gmail.com>
2020-03-04 00:44:11 +0200

    set styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import moment from "moment";
+// import moment from "moment";
+// import Summary from "./components/Summary";
+import HomePage from "./components/homePage/HomePage";
 import "./App.css";
-
-import Summary from "./components/Summary";
-import CalcForm from './components/calcForm/CalcForm'
-
 
 function App() {
   return (
-    // <CalcForm/>
-    <Summary />
-  )
+    <>
+      <HomePage />
+      {/* <Summary /> */}
+    </>
+  );
 }
 
 export default App;
