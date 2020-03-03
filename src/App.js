90ce5e9d170dd6d99e00287bbf18cb15e4e7cf57
Roman <roman.svitelskyi@gmail.com>
2020-03-03 12:37:51 +0200

    change in Summary

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import Summary from "./components/Summary";
 
 function App() {
-  return <p>Slim Mom</p>;
+  return (
+    <>
+      <p>Slim Mom</p>
+      <Summary />
+    </>
+  );
 }
 
 export default App;
