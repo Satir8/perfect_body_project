9e91c4c721c49dae3e22cb3a257b7fa490f2278f
YK911 <jaroslavkositsa@gmail.com>
2020-03-04 14:32:26 +0200

    add overlay to loader

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from "react";
 import "./Loader.css";
 
-export const Loader = () => <div className="loaderImg"></div>;
+export const Loader = () => (
+  <div className="overlay">
+    <div className="loaderImg"></div>
+  </div>
+);
