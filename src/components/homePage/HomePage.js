16d169e1438cf0caeb792b432a1c4e59033bd751
YK911 <jaroslavkositsa@gmail.com>
2020-03-04 10:24:08 +0200

    intermediate option

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,16 +1,16 @@
 import React from "react";
-// import CalcForm from "../calcForm/CalcForm";
+import CalcForm from "../calcForm/CalcForm";
 import logo from "../../images/logo.png";
 import "./HomePage.css"
 
 const HomePage = () => {
   return (
-    <div className="homePageContainer">
+    <div className="homePageWrapper">
       <div className="homePageHeader">
         <img src={logo} alt="header" width="320" height="80" />
       </div>
       <div className="homePageBody">
-        {/* <CalcForm /> */}
+        <CalcForm />
       </div>
     </div>
   );
