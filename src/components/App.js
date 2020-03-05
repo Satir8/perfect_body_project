45b32e933667267c3e0df0f61c534c6a0fb24447
Gregory Yakovina <54667797+Satir8@users.noreply.github.com>
2020-03-05 12:13:20 +0300

    merged graph

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
-import Header from './header/Header';
-import { Switch, Route } from 'react-router-dom';
-import NavPage from '../pages/navPage/NavPage';
-import Achievements from './achievements/Achievements';
+import React, { Component } from "react";
+import Header from "./header/Header";
+import { Switch, Route } from "react-router-dom";
+import NavPage from "../pages/navPage/NavPage";
+import Achievements from "./achievements/Achievements";
+import HomePage from "../pages/homePage/HomePage";
+import DashboardPage from "../pages/dashboardPage/DashboardPage";
 
 class App extends Component {
   state = {
@@ -38,18 +40,22 @@ class App extends Component {
   };
 
   render() {
-    const { isMobile, isTablet, isDesktop } = this.state
+    const { isMobile, isTablet, isDesktop } = this.state;
     return (
       <>
+        <DashboardPage />
         <Header isMobile={isMobile} isTablet={isTablet} isDesktop={isDesktop} />
         <Switch>
-          <Route exact path='/' component={() => <div>Home</div>} />
-          <Route path='/nav' component={NavPage} />
+          <Route exact path="/" component={() => <HomePage />} />
+          <Route path="/nav" component={NavPage} />
           <Route
-            path='/authorization'
+            path="/authorization"
             component={() => <div>Login/Signup</div>}
           />
-          <Route path='/achievements' render={props => <Achievements {...props} isMobile={isMobile} />} />
+          <Route
+            path="/achievements"
+            render={props => <Achievements {...props} isMobile={isMobile} />}
+          />
         </Switch>
       </>
     );
