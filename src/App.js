a1d61a0e2f8598f3f841fe2f7ab307ea45853d89
Roman <roman.svitelskyi@gmail.com>
2020-03-06 09:53:41 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { lazy } from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homePage/HomePage";
 // import DashboardPage from "./pages/dashboardPage/DashboardPage";
-// import Summary from "./components/summary/Summary";
+import Summary from "./components/summary/Summary";
 // import CalcForm from "./components/calcForm/CalcFormContainer";
 import "./App.css";
 
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <>
       <HomePage />
-      {/* <Summary /> */}
+      <Summary />
     </>
   );
 };
