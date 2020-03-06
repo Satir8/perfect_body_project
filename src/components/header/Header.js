70c787bfdbc9256fd5fcca2ab62289251d911213
a-sapon <sapon.anya@gmail.com>
2020-03-06 13:45:07 +0200

    added logout modal

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,33 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Header.module.css';
 import Burger from './Burger';
 import NavPage from '../../pages/navPage/NavPage';
 import { connect } from 'react-redux';
+import ModalLogout from '../modalLogout/modalLogout';
 
 const burgerIcon = styles.hrdBurger;
 const closeIcon = styles.hrdBurgerClose;
 
-const Header = ({ isMobile, isTablet, isDesktop }) => (
-  <div className={styles.hdrContainer}>
-    <p className={styles.hdrLogo}>
-      Slim<span>Mom</span>
-    </p>
-    {isMobile && <Burger burgerIcon={burgerIcon} closeIcon={closeIcon} />}
-    {isTablet && (
-      <>
-      <ul className={styles.hdrAuthList}>
-        <li className={styles.hdrAuthListItem}>Nicname</li>
-        <li className={styles.hdrAuthListItem}>Выйти</li>
-      </ul>
-      <Burger burgerIcon={burgerIcon} closeIcon={closeIcon} />
-      </>
-    )}
-    {isDesktop && <NavPage />}
-  </div>
-);
+class Header extends Component {
+  state = {
+    showExitModal: false
+  };
 
-const mapStateToProps = (state) => ({
-  
+  openExitModal = () => {
+    this.setState({showExitModal: true})
+  }
+
+  closeModal = () => {
+    this.setState({showExitModal: false});
+  };
+
+
+  render() {
+    console.log(this.state);
+    const { showExitModal } = this.state
+    const { isMobile, isTablet, isDesktop, auth, nicname } = this.props;
+    return (
+      <>
+      <div className={styles.hdrContainer}>
+        <p className={styles.hdrLogo}>
+          Slim<span>Mom</span>
+        </p>
+        {isMobile && <Burger burgerIcon={burgerIcon} closeIcon={closeIcon} />}
+        {isTablet && (
+          <>
+            <div className={styles.hdrAuthList}>
+              {true ? (
+                <>
+                  <p
+                    className={[
+                      styles.hdrAuthListItem,
+                      styles.hdrAuthListItemBold
+                    ].join(' ')}
+                  >
+                    nicname
+                  </p>
+                  <p
+                    onClick={() => this.openExitModal()}
+                    className={[
+                      styles.hdrAuthListItem,
+                      styles.hdrAuthListItemLink
+                    ].join(' ')}
+                  >
+                    Выйти
+                  </p>
+                </>
+              ) : (
+                <>
+                  <p
+                    className={[
+                      styles.hdrAuthListItem,
+                      styles.hdrAuthListItemLink
+                    ].join(' ')}
+                  >
+                    Вход
+                  </p>
+                  <p
+                    className={[
+                      styles.hdrAuthListItem,
+                      styles.hdrAuthListItemLink
+                    ].join(' ')}
+                  >
+                    Регистрация
+                  </p>
+                </>
+              )}
+            </div>
+            <Burger burgerIcon={burgerIcon} closeIcon={closeIcon} />
+          </>
+        )}
+        {isDesktop && <NavPage />}
+      </div>
+      {showExitModal && <ModalLogout closeModal={this.closeModal} />}
+      </>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  auth: state.session.authenticated,
+  nicname: state.session.user
 });
 
 export default connect(mapStateToProps)(Header);
--- src/components/modalLogout/modalLogout.jsx
+++ src/components/modalLogout/modalLogout.jsx
@@ -1,46 +1,27 @@
-import React, { Component } from "react";
-import styles from "./modalLogout.module.css";
+import React from 'react';
+import styles from './modalLogout.module.css';
 
-class ModalLogout extends Component {
-  state = { isOpen: false };
+const modalLogout = ({ closeModal }) => (
+  <>
+    <div className={styles.centerCase}>
+      <div className={styles.moduleCase}>
+        <img
+          alt='close'
+          onClick={() => closeModal()}
+          className={styles.btnClose}
+          src='https://avatanplus.com/files/resources/mid/5968a2c8f2ed115d40bbe123.png'
+        />
+        <p className={styles.text}>Ты дейсвтительно хочешь выйти?</p>
+        <hr className={styles.line}></hr>
+        <div className={styles.btnCase}>
+          <button className={styles.btnYes}>Да</button>
+          <button onClick={() => closeModal()} className={styles.btnNo}>
+            Нет, я хочу остаться
+          </button>
+        </div>
+      </div>
+    </div>
+  </>
+);
 
-  openModal = () => {
-    this.setState(prev => ({ isOpen: !prev.isOpen }));
-  };
-
-  closeModal = () => {
-    this.setState(prev => ({ isOpen: !prev.isOpen }));
-  };
-  render() {
-    return (
-      <>
-        <button onClick={() => this.openModal()}>Logout</button>
-        {this.state.isOpen && (
-          <div className={styles.centerCase}>
-            <div className={styles.moduleCase}>
-              <img
-                alt="close"
-                onClick={() => this.closeModal()}
-                className={styles.btnClose}
-                src="https://avatanplus.com/files/resources/mid/5968a2c8f2ed115d40bbe123.png"
-              />
-              <p className={styles.text}>Ты дейсвтительно хочешь выйти?</p>
-              <hr className={styles.line}></hr>
-              <div className={styles.btnCase}>
-                <button className={styles.btnYes}>Да</button>
-                <button
-                  onClick={() => this.closeModal()}
-                  className={styles.btnNo}
-                >
-                  Нет, я хочу остаться
-                </button>
-              </div>
-            </div>
-          </div>
-        )}
-      </>
-    );
-  }
-}
-
-export default ModalLogout;
+export default modalLogout;
